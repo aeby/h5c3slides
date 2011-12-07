3def9f7244c30c195901435a437c8e231007ebb4
Reto Aebersold <aeby@atizo.com>
2011-12-07 17:42:20 +0100

    add js infos

--- h5c3/js/introduction.js
+++ h5c3/js/introduction.js
@@ -2,6 +2,21 @@ $(function() {
 	// Deck initialization
 	$.deck('.slide');
 	
-	SyntaxHighlighter.all()
+	SyntaxHighlighter.all();
+	
+	$(window).bind('message', function(e){
+		$.deck('go', e.originalEvent.data);
+	});
+	
+	$(window).bind('keypress', function(e) {
+		var code = (e.keyCode ? e.keyCode : e.which);
+		var loc = document.location.href;
+        if(code === 112){ // p
+        	var presentation = window.open(loc);
+        	presentation.focus();
+        	$(document).bind('deck.change', function(event, from, to) {
+        		presentation.postMessage(to - 1, loc);        	
+        	});        
+        }
+	});
 });
-
